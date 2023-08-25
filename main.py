f23cf970f9ca63da40e6c75045666ea526a8e966
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-08-25 11:10:35 +0200

    added solution

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 def sort(numbers):
-    pass
+
+    for j in range(len(numbers)):
+        for i in range(len(numbers)-1):
+            if numbers[i] > numbers[i+1]:
+                numbers[i], numbers[i + 1] = numbers[i+1], numbers[i]
+
 
 
 if __name__ == '__main__':
     numbers = [64, 34, 25, 12, 22, 11, 90]
+    print(numbers)
     sort(numbers)
     print(numbers)
