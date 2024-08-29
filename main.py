7c2efb4d2a9fb27d611d82b865876cd6bfa7c788
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-08-29 15:56:12 +0200

    asdf

--- main.py
+++ main.py
@@ -10,13 +10,12 @@ def sort(numbers):
     :return:
     """
     for _ in range(len(numbers)):
-        for i in range(len(numbers)-1):
-            if numbers[i] > numbers[i+1]:
-                numbers[i], numbers[i + 1] = numbers[i+1], numbers[i]
+        for i in range(len(numbers) - 1):
+            if numbers[i] > numbers[i + 1]:
+                numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     num = [64, 34, 25, 12, 22, 11, 90]
     print(num)
     sort(num)
--- main_test.py
+++ main_test.py
@@ -1,10 +1,12 @@
 import main
 
+
 def test_sort_presorted():
     # Testfall 1: Testen einer bereits sortierten Liste
     numbers = [11, 22, 34, 45, 64, 90]
     main.sort(numbers)
     assert numbers == [11, 22, 34, 45, 64, 90], "Testfall 1 fehlgeschlagen"
+
 
 def test_sort_inverse():
     # Testfall 2: Testen einer umgekehrt sortierten Liste
