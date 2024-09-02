a102e4c196fb95e15123d2c8591e768a526e4e83
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-02 09:34:10 +0200

    asdf

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def sort(numbers):
     for _ in range(len(numbers)):
         for i in range(len(numbers) - 1):
             if numbers[i] > numbers[i + 1]:
+                #
                 numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
 
 
